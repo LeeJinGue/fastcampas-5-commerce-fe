bcede8622e1d4c0b6312a5b547968a6bbce445ee
LeeJinGue <hippo9851@gmail.com>
2022-09-26 17:05:32 +0900

    feat: 회원가입 성공시 회원가입 성공 페이지로 넘어가도록 구현

--- src/components/SignupPage/SignupPage.tsx
+++ src/components/SignupPage/SignupPage.tsx
@@ -1,13 +1,17 @@
+import { useRouter } from 'next/router';
 import SignupPageContentView from './SignupPage.view';
 import useExampleForm from './_hooks/useSignupForm';
 
 const SignupPage = () => {
   const formData = useExampleForm();
+  const route = useRouter()
   const { handleSubmit } = formData;
   const onSubmit = handleSubmit(({ username,nickname, email, phone, gender, age,tos }) => {
+    // 회원가입 성공!
     console.log(
       `submitted: ${username}, ${nickname} , ${email}, ${phone}, ${gender.value}, ${age.value}, ${tos.service}, ${tos.privacy}, ${tos.marketing}`,
     );
+    route.replace('/signup/success')
   });
 
   return <SignupPageContentView formData={formData} onSubmit={onSubmit} />;
