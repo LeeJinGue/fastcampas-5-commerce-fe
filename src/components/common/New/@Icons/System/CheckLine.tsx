df86b7c6cbb5ef17dfd064d267ef79396e52306a
LeeJinGue <hippo9851@gmail.com>
2022-09-27 01:46:12 +0900

    chore: CheckLineIcon prop 추가
    - isChecked를 통해 선택 된 아이콘을 넘겨줄지 결정

--- src/components/common/New/@Icons/System/CheckLine.tsx
+++ src/components/common/New/@Icons/System/CheckLine.tsx
@@ -1,15 +1,20 @@
 import { Icon, IconProps } from '@chakra-ui/react';
-
-const CheckLineIcon = ({...props }) => {
+interface CheckIconProps extends IconProps{
+  isIndeterminate?: boolean,
+  isChecked?: boolean,
+}
+const CheckLineIcon = ({...props }:CheckIconProps) => {
   const w = props.w? props.w : "24"
   const h = props.h? props.h : "24"
   const { isIndeterminate, isChecked, ...rest } = props
   if(isChecked) return <Icon
   bg="white"
+  {...rest}
   width={w+"px"} height={h+"px"} viewBox={`0 0 ${w} ${h}`} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
   <path d="M5.84302 11.33L10.532 17.2469L17.465 7.51001" stroke="#FF710B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </Icon>
   return <Icon bg="white"
+  {...rest}
   width={w+"px"} height={h+"px"} viewBox={`0 0 ${w} ${h}`} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
   <path d="M5.84302 11.33L10.532 17.2469L17.465 7.51001" stroke="#CBCED6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </Icon>
