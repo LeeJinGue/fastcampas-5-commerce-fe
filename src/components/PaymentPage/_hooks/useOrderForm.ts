4e7ca3ee3af7880267ce9ada541a7715e32f26eb
LeeJinGue <hippo9851@gmail.com>
2022-10-04 03:12:02 +0900

    feat: 주문결제 페이지 추가
    - 주문자 정보 / 배송지 정보 입력할 Form 관리를 위한 useOrerForm Hook 추가
    - 주문결제 페이지 디자인을 관리하는 PaymentPage.view 추가

---
+++ src/components/PaymentPage/_hooks/useOrderForm.ts
@@ -0,0 +1,119 @@
+import { UseFormProps, useForm } from 'react-hook-form';
+
+import * as yup from 'yup';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+
+export type OrderFormDataType = {
+  order: {
+    name: string;
+    phone: string;
+    address: {
+      zipcode: string;
+      detail: string;
+    };
+  },
+  delivery: {
+    name: string;
+    phone: string;
+    address: {
+      zipcode: string;
+      detail: string;
+    };
+  }
+  deliveryrequest: string;
+};
+const initialFormData:OrderFormDataType = {
+  order: {
+    name: "",
+    phone: "",
+    address: {
+      zipcode: "",
+      detail: "",
+    },
+  },
+  delivery: {
+    name: "",
+    phone: "",
+    address: {
+      zipcode: "",
+      detail: "",
+    },
+  },
+  deliveryrequest: "",
+}
+/**
+ * yup 을 이용하여 form의 유효성 검사를 도와줍니다.
+ * react-hook-form과 yup을 연결해 줄 yupResolver 을 함께 사용합니다.
+ *
+ * validation에 반복되는 값은 상수로 빼서 관리합니다.
+ *
+ *
+ *
+ * @see https://github.com/jquense/yup#getting-started
+ * @see https://yarnpkg.com/package/@hookform/resolvers#readme
+ * */
+export const OrderFormSchema = yup.object().shape({
+  order: yup.object().shape({
+    name: yup.string()
+    .required('해당 항목은 필수값 입니다.')
+    .min(2, "최소 2자 이상 입력해주세요."),
+    phone: yup
+      .string()
+      .required('해당 항목은 필수값 입니다.')
+      .test(
+        'isPhone',
+        '정확히 핸드폰 번호를 입력해주세요.',
+        (val) => {
+          if(val !== undefined){
+            const regPhone = /\d{2,3}\-\d{3,4}\-\d{4}/
+            const testPhone = regPhone.test(val)
+            return testPhone
+          }
+          console.log("# 핸드폰 입력 에러")
+          return false
+        },
+      ),
+    address: yup.object().shape({
+      zipcode: yup.string().required('해당 항목은 필수값 입니다.'),
+      detail: yup.string().required('해당 항목은 필수값 입니다.'),
+    }),
+  }),
+  delivery: yup.object().shape({
+    name: yup.string()
+    .required('해당 항목은 필수값 입니다.')
+    .min(2, "최소 2자 이상 입력해주세요."),
+    phone: yup
+      .string()
+      .required('해당 항목은 필수값 입니다.')
+      .test(
+        'isPhone',
+        '정확히 핸드폰 번호를 입력해주세요.',
+        (val) => {
+          if(val !== undefined){
+            const regPhone = /\d{2,3}\-\d{3,4}\-\d{4}/
+            const testPhone = regPhone.test(val)
+            return testPhone
+          }
+          console.log("# 핸드폰 입력 에러")
+          return false
+        },
+      ),
+    address: yup.object().shape({
+      zipcode: yup.string().required('해당 항목은 필수값 입니다.'),
+      detail: yup.string().required('해당 항목은 필수값 입니다.'),
+    }),
+  }),
+  deliveryrequest: yup.string().required('해당 항목은 필수값 입니다.'),
+});
+
+const useOrderForm = (options?: UseFormProps<OrderFormDataType>) => {
+  return useForm<OrderFormDataType>({
+    resolver: yupResolver(OrderFormSchema),
+    mode: 'onChange',
+    defaultValues: initialFormData,
+    ...options,
+  });
+};
+
+export default useOrderForm;
