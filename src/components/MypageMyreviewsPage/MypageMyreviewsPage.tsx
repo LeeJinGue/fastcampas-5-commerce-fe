f1a85ba606e1b2243e54f8cc155b03830a6feb4d
LeeJinGue <hippo9851@gmail.com>
2022-10-05 22:36:49 +0900

    refactor: 상품 상세 페이지에서 API 연동
    - Product/Review DTOT 타입 추가해서 변수 관리
    - 기존 더미데이터 타입을 실제 받아오는 데이터 타입인 DTOT 타입으로 변경

--- src/components/MypageMyreviewsPage/MypageMyreviewsPage.tsx
+++ src/components/MypageMyreviewsPage/MypageMyreviewsPage.tsx
@@ -4,16 +4,22 @@ import { LAYOUT } from '@constants/layout';
 import Review from '@components/common/Card/Review';
 import { review_list } from '@constants/dummy';
 import Pagination from '@components/common/New/Pagination';
+import { useGetProductByIdQuery } from '@apis/product/ProductApi.query';
 
 interface MypageMyreviewsPageProps extends ChakraProps {}
 
 function MypageMyreviewsPage({ ...basisProps }: MypageMyreviewsPageProps) {
+  // 아직 유저 아이디로 review받아오는 코드 추가 안해서 돌려서 Product를 통해 review리스트 받아와서 사용
+  const data = useGetProductByIdQuery({variables:"1"})
+  console.log("#data test:",data)
+  const reviewList = data.data?.reviewList
+
   return (
     <Flex pt={LAYOUT.HEADER.HEIGHT} pb="80px" flexDir="column" bgColor="white" w="375px"{...basisProps}>
       <Text px="16px" mt="50px" textStyle="titleLarge" textColor="black">내 상품 리뷰</Text>
       <Text px="16px" mt="80px" textStyle="title" textColor="black">{"총 "}<Text as="span" textColor="primary.500">{"78"}</Text>건</Text>
       <Flex flexDir="column" mt="30px">
-        {review_list.map((value) => {
+        {reviewList && reviewList.map((value) => {
           return <Review iscomment={false} reviewData={value}/>
         })}
       </Flex>
