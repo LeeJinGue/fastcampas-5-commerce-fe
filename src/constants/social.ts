beeb4233166b6b1e225a214a59d4a8143e3a8875
LeeJinGue <hippo9851@gmail.com>
2022-10-05 20:51:36 +0900

    feat: social파일에 클라이언트 ID 및 리다이렉트 URL 추가

--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,7 +1,11 @@
 export const SOCIAL = {
-  KAKAO_CLIENT_ID: '',
+  KAKAO_CLIENT_ID: '55ad0e25826e8e1528cbf532932d9a87',
+  KAKAO_CLIENT_ID_ME: 'b4c6d57034e2f5cf3b74648cf373863d',
   NAVER_CLIENT_ID: '',
   FACEBOOK_CLIENT_ID: '',
   GOOGLE_CLIENT_ID: '',
   APPLE_CLIENT_ID: '',
+  REDIRECT_URL_ME: 'http://localhost:3000/social_login/callback',
+  REDIRECT_URL: 'http://127.0.0.1:3000/social_login/callback',
+  
 };
