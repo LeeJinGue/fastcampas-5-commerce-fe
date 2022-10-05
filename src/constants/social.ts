a9fa48dcaf880be5f7c3d7bc67a21b9bdd1a45ce
LeeJinGue <hippo9851@gmail.com>
2022-10-05 22:37:43 +0900

    fix: react query 테스트를 위해 Devtool 추가

--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,11 +1,13 @@
+import { CONFIG } from "@config";
+
 export const SOCIAL = {
-  KAKAO_CLIENT_ID: '',
-  KAKAO_CLIENT_ID_ME: '',
+  KAKAO_CLIENT_ID: CONFIG.AUTH_KAKAO_CLIENT_KEY,
+  KAKAO_CLIENT_ID_ME: CONFIG.AUTH_KAKAO_MYKEY,
   NAVER_CLIENT_ID: '',
   FACEBOOK_CLIENT_ID: '',
   GOOGLE_CLIENT_ID: '',
   APPLE_CLIENT_ID: '',
   REDIRECT_URL_ME: 'http://127.0.0.1:3000/social_login/callback',
-  REDIRECT_URL: 'http://127.0.0.1:3000/social_login/callback',
+  REDIRECT_URL: `http://localhost:3000/social_login/callback`,
   
 };
