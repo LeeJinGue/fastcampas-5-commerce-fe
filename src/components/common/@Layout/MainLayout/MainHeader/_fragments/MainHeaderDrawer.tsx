1340c8f3c8cf979a472f2186f05f545aee816986
LeeJinGue <hippo9851@gmail.com>
2022-10-07 19:33:45 +0900

    feat: Drawer Menu 이벤트 추가
    - 홈, 상품보기, 마이페이지 이동 및 로그아웃 버튼들에 onClick 이벤트 추가

--- src/components/common/@Layout/MainLayout/MainHeader/_fragments/MainHeaderDrawer.tsx
+++ src/components/common/@Layout/MainLayout/MainHeader/_fragments/MainHeaderDrawer.tsx
@@ -1,4 +1,4 @@
-import { Button, ChakraProps, Container, DrawerFooter, DrawerHeader, DrawerProps, Flex, HStack, List, MenuList, VStack } from '@chakra-ui/react';
+import { Button, ChakraProps, Container, DrawerFooter, DrawerHeader, DrawerProps, Flex, HStack, List, MenuList, useDisclosure, VStack } from '@chakra-ui/react';
 import {
   Drawer,
   DrawerBody,
@@ -7,9 +7,12 @@ import {
   DrawerOverlay,
 } from '@chakra-ui/react';
 import ExitIcon from '@components/common/@Icons/System/Exit';
-import DrawerMenuList from '@components/common/DrawerMenuList';
+import DrawerMenuList, { menuItemType } from '@components/common/DrawerMenuList';
 import { DRAWER_MENU_ITEM_LIST } from '@constants/string';
 import { LAYOUT } from '@constants/layout';
+import { useRouter } from 'next/router';
+import { deleteToken } from '@utils/localStorage/token';
+import { ROUTES } from '@constants/routes';
 
 interface MainHeaderDrawerProps extends Omit<DrawerProps, 'children'> {
   bodyProps?: ChakraProps;
@@ -18,7 +21,31 @@ interface MainHeaderDrawerProps extends Omit<DrawerProps, 'children'> {
 const MainHeaderDrawer = ({
   bodyProps,
   ...basisProps
-}: MainHeaderDrawerProps) => (
+}: MainHeaderDrawerProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const route = useRouter()
+  const handleLogout = () => {
+    // token을 삭제하고 login화면으로 이동합니다.
+    deleteToken()
+    route.replace({pathname: ROUTES.LOGIN})
+  }
+  const goHome = () => {
+    onClose()
+    route.replace({pathname:ROUTES.HOME})
+  }
+  const goProduct = () => {
+    onClose()
+    route.push({pathname:ROUTES.PRODUCTS})
+  }
+  const goMypage = () => {
+    onClose()
+    route.push({pathname:ROUTES.MYPAGE.MAIN})
+  }
+  const FUNCTION_LIST = [goHome, goProduct, goMypage]
+  const menuItemList:menuItemType[] = [] 
+  for(let i=0; i<DRAWER_MENU_ITEM_LIST.length;i++) menuItemList.push({name: DRAWER_MENU_ITEM_LIST[i], event: FUNCTION_LIST[i]})
+
+  return (
   <Drawer placement="left" {...basisProps}>
     <DrawerOverlay />
     <DrawerContent w="313px" maxW="313px">
@@ -33,13 +60,14 @@ const MainHeaderDrawer = ({
           right="16px"
           onClick={basisProps.onClose} />
         <Flex px="16px" mt={LAYOUT.HEADER.HEIGHT} fontFamily="heading" textStyle="titleLarge" mb="30px">카테고리</Flex>
-        <DrawerMenuList menuItems={DRAWER_MENU_ITEM_LIST} />
+        <DrawerMenuList menuItems={menuItemList} />
       </DrawerBody>
       <DrawerFooter px="16px" py="25px" display="flex" justifyContent="start">
-      <Button bg="white" textStyle="titleLarge" leftIcon={<ExitIcon/>}>로그아웃</Button>
+      <Button bg="white" textStyle="titleLarge" onClick={handleLogout} leftIcon={<ExitIcon/>}>로그아웃</Button>
       </DrawerFooter>
     </DrawerContent>
   </Drawer>
 );
+  }
 
 export default MainHeaderDrawer;
