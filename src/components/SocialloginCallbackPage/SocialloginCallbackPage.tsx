1ebafb6134fa9019529a04366031cc56046d61d7
LeeJinGue <hippo9851@gmail.com>
2022-10-11 23:33:32 +0900

    feat: Social Login시 Spinner 추가

--- src/components/SocialloginCallbackPage/SocialloginCallbackPage.tsx
+++ src/components/SocialloginCallbackPage/SocialloginCallbackPage.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Box, ChakraProps, Button, Flex, Image, Text } from '@chakra-ui/react';
+import { Box, ChakraProps, Flex, Spinner } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import axios from 'axios';
-import { SOCIAL } from '@constants/social';
 import { setToken } from '@utils/localStorage/token';
-import { usePostRegisterMutation, usePostUserSocialLogin } from '@apis/user/UserApi.mutation';
+import { usePostUserSocialLogin } from '@apis/user/UserApi.mutation';
 import { ROUTES } from '@constants/routes';
 interface SocialloginCallbackPageProps extends ChakraProps { }
-const grant_type = "authorization_code"
-const client_id = SOCIAL.KAKAO_CLIENT_ID
-const redirect_url = SOCIAL.REDIRECT_URL
 function SocialloginCallbackPage({
   ...basisProps
 }: SocialloginCallbackPageProps) {
@@ -19,14 +14,15 @@ function SocialloginCallbackPage({
   React.useEffect(() => {
     // Redirect URL을 통해 카카오 로그인
     if (typeof code === "string" && typeof state === "string") {
-      console.log("#code:", code)
-      socialLoginMutate({ code:code, state:state })
+      // console.log("#code:", code)
+      socialLoginMutate({ code: code, state: state })
         .then((res) => {
           const { isRegister } = res
           if (!isRegister) {
             // 등록이 안되어있다면 
             // socialToken을 Query로 넘기면서 
             // 회원가입 페이지로 이동합니다.
+            // console.log("등록이 안되어있습니다.")
             const { socialToken } = res
             if (socialToken) {
               route.push({
@@ -41,6 +37,7 @@ function SocialloginCallbackPage({
             // 등록이 되어있다면
             // accessToken, refreshToken을 저장하고
             // 메인 페이지로 이동합니다.
+            // console.log("등록이 되어있습니다.")
             const { access, refresh } = res
             if (access && refresh) {
               setToken({ isRegister, access, refresh })
@@ -56,9 +53,14 @@ function SocialloginCallbackPage({
   }, [code])
 
   return (
-    <Box {...basisProps}>
-      <Text>SocialloginCallbackPage</Text>
-    </Box>
+    <Flex w="375px" h="782px" justifyContent="center" alignItems="center" bgColor="white" {...basisProps} >
+      <Spinner thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='primary.500' 
+        w="70px" h="70px"
+        size="xl"/>
+    </Flex>
   );
 }
 
