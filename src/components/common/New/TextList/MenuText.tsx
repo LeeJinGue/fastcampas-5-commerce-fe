b929b79fc2864dbc734776a986b3ad1e74b46fac
LeeJinGue <hippo9851@gmail.com>
2022-10-11 13:07:13 +0900

    feat: Mypage에 버튼 이벤트 추가
    - 회원정보 수정, 주문내역 내 상품리뷰 페이지 이동 이벤트 추가
    - 회원탈퇴, 로그아웃 페이지 이동 이벤트 추가

--- src/components/common/New/TextList/MenuText.tsx
+++ src/components/common/New/TextList/MenuText.tsx
@@ -1,16 +1,19 @@
-import { Flex, FlexProps, Text } from '@chakra-ui/react';
+import { Button, Flex, FlexProps, Text } from '@chakra-ui/react';
 import React from 'react'
 import ListHorizontalArrowIcon from '../@Icons/System/ListHorizontalArrow';
 import ListNumberArrowIcon from '../@Icons/System/ListNumberArrow';
 interface MenuTextProps extends FlexProps{
   title: string,
+  event: ()=>void
 }
-function MenuText({title, ...props}:MenuTextProps) {
+function MenuText({title, event,...props}:MenuTextProps) {
   return (
     <Flex {...props} w="375px" h="60px" px="16px" 
     alignItems="center" justifyContent="space-between">
       <Text textStyle="text" textColor="black">{title}</Text>
-      <ListHorizontalArrowIcon state={true} colortype={'Black'} />
+      <Button bgColor="white" onClick={event}>
+        <ListHorizontalArrowIcon state={"true"} colortype={'Black'} />
+      </Button>
     </Flex>
   )
 }
