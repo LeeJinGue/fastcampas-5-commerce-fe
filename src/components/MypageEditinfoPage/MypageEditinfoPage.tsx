f925218162e39ce7d2c5883a1554a74912b3ac1e
LeeJinGue <hippo9851@gmail.com>
2022-10-11 20:34:43 +0900

    feat: 회원정보수정 페이지 기존 정보 받아오도록 구현
    - 메인 화면에서 /user/me/ API 요청을 통해 유저 데이터를 redux에 저장해둔다.
    - 회원정보 수정 페이지에서 redux에 저장된 데이터를 기반으로 default value를 설정한다.
    - 저장 버튼 클릭시 API요청을 통해 폼에 입력된 유저 데이터를 서버에 저장한다.
    - 성별, 연령 옵션 배열을 form.ts파일로 옮겼다.

--- src/components/MypageEditinfoPage/MypageEditinfoPage.tsx
+++ src/components/MypageEditinfoPage/MypageEditinfoPage.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Box, ChakraProps, Button, Flex, Image, Text } from '@chakra-ui/react';
 import { LAYOUT } from '@constants/layout';
-import useEditInfoForm from './_hooks/useEditInfoForm';
+import useEditInfoForm, { EditInfoFormDataType } from './_hooks/useEditInfoForm';
 import { useRouter } from 'next/router';
 import EditInfoPageView from './MypageEditinfoPage.view';
+import { useGetUserMeQuery } from '@apis/user/UserApi.query';
+import { getToken } from '@utils/localStorage/token';
+import { useDispatch } from 'react-redux';
+import store from '@features/store';
+import { usePatchUserMe } from '@apis/user/UserApi.mutation';
+import { PROFILE_EXAMPLE } from '@constants/string';
+import { ROUTES } from '@constants/routes';
 
-interface MypageEditmyinfoPageProps extends ChakraProps {}
+interface MypageEditmyinfoPageProps extends ChakraProps { }
 
 function MypageEditmyinfoPage({ ...basisProps }: MypageEditmyinfoPageProps) {
   const formData = useEditInfoForm();
   const route = useRouter()
   const { handleSubmit } = formData;
-  const onSubmit = handleSubmit(({ username,nickname, email, phone, gender, age }) => {
+  const { mutateAsync:editInfoMutate, isLoading, isSuccess, data} = usePatchUserMe()
+  const onSubmit = handleSubmit(({ username, nickname, email, phone, gender, age }) => {
     // 회원정보 수정 성공!
-    console.log(
-      `submitted: ${username}, ${nickname} , ${email}, ${phone}, ${gender.value}, ${age.value}`,
-    );
-    route.replace('/signup/success')
+    const phoneData= phone.replaceAll("-", "")
+    editInfoMutate({
+      name: username,
+      nickname,
+      phone: phoneData,
+      email,
+      profile: PROFILE_EXAMPLE,
+      gender: gender.value,
+      age: Number.parseInt(age.value),
+      accessToken: getToken()?.access!
+    }).then((res) => {
+      // console.log("회원정보 수정 성공, ",res)
+    }).catch((err) => {
+      // console.log("회원정보 수정 에러, ",err)
+    })
   });
-
   return <EditInfoPageView {...basisProps} formData={formData} onSubmit={onSubmit} />;
+
 }
 
 export default MypageEditmyinfoPage;
---
+++ src/constants/form.ts
@@ -0,0 +1,12 @@
+export const GENDER_OPTION = [
+  { value: 'male', label: '남자' },
+  { value: 'female', label: '여자' },
+]
+export const AGE_OPTION = [
+  { value: '10', label: '10대' },
+  { value: '20', label: '20대' },
+  { value: '30', label: '30대' },
+  { value: '40', label: '40대' },
+  { value: '50', label: '50대' },
+  { value: '60', label: '60대' },
+];
