414d71921d6bad0a4eb855297d30c343a3c2e47a
LeeJinGue <hippo9851@gmail.com>
2022-10-12 22:48:48 +0900

    feat: 로딩 페이지 구현

--- src/pages/products/[id]/index.tsx
+++ src/pages/products/[id]/index.tsx
@@ -4,18 +4,33 @@ import ProductsDetailByIdPage from '@components/ProductsDetailByIdPage';
 import MainLayout from '@components/common/@Layout/MainLayout';
 import { useEffect } from 'react';
 import { useGetProductByIdQuery } from '@apis/product/ProductApi.query';
+import LoadingPage from '@components/common/New/LoadingPage';
 
 function ProductsDetailById() {
   const router = useRouter();
   const { id } = router.query;
   if(!id || typeof id === "object") return
+  const data = useGetProductByIdQuery({ variables: id })
+  if (data.isSuccess) {
+    const productData = data.data
+    return (
+      <>
+        <Head>
+          {/* ex) Your App Name | Page Name */}
+          <title>인코스런 커머스트랙 | 상품 상세보기</title>
+        </Head>
+        <MainLayout content={<ProductsDetailByIdPage productData={productData} />} />
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
         {/* ex) Your App Name | Page Name */}
         <title>인코스런 커머스트랙 | 상품 상세보기</title>
       </Head>
-      <MainLayout content={<ProductsDetailByIdPage id={id} />} />
+      <MainLayout content={<LoadingPage /> } />
     </>
   );
 }
