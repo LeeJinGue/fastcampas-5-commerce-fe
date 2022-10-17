3ad2dddec6f48a7e394d9e7da2e760ada6df3be4
LeeJinGue <hippo9851@gmail.com>
2022-10-18 03:06:21 +0900

    fix: 유저 정보 없이 getCart 가능하도록 수정 - useQuery할 때 token으로 스스로 user정보 갖고와서 CartApi불러오도록 수정

--- src/apis/cart/CartApi.query.ts
+++ src/apis/cart/CartApi.query.ts
@@ -1,4 +1,7 @@
+import userApi from '@apis/user/UserApi';
+import { useGetUserMeQuery } from '@apis/user/UserApi.query';
 import { useQuery } from '@tanstack/react-query';
+import { getToken } from '@utils/localStorage/token';
 
 import { QueryHookParams } from '../type';
 
@@ -11,12 +14,18 @@ export const Cart_API_QUERY_KEY = {
 };
 
 export function useGetCartQuery(
-  params?: QueryHookParams<typeof CartApi.getCart>,
+  params: QueryHookParams<typeof CartApi.getCart>,
 ) {
   const queryKey = Cart_API_QUERY_KEY.GET(params?.variables);
   const query = useQuery(
     queryKey,
-    () => CartApi.getCart(params?.variables),
+    async () => {
+      const accessToken = getToken()!.access
+      const userData = await userApi.getUserMe({accessToken})
+      // return CartApi.getCart(params.variables)
+      const user_id = userData.id
+      return CartApi.getCart({user_id})
+    },
     params?.options,
   );
   return { ...query, queryKey };
