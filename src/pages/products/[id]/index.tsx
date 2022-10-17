fdd761bff89b2a0f1d61c9d4775ff2cf3c0a9136
LeeJinGue <hippo9851@gmail.com>
2022-10-18 03:11:55 +0900

    fix: 상품 상세 페이지에서 index에서 상품정보, 유저정보 내려주도록 변경

--- src/pages/products/[id]/index.tsx
+++ src/pages/products/[id]/index.tsx
@@ -5,23 +5,34 @@ import MainLayout from '@components/common/@Layout/MainLayout';
 import { useEffect } from 'react';
 import { useGetProductByIdQuery } from '@apis/product/ProductApi.query';
 import LoadingPage from '@components/common/New/LoadingPage';
+import { useGetUserMeQuery } from '@apis/user/UserApi.query';
+import { getToken } from '@utils/localStorage/token';
+import { useGetCartQuery } from '@apis/cart/CartApi.query';
+import { usePostCartMutation } from '@apis/cart/CartApi.mutation';
 
 function ProductsDetailById() {
   const router = useRouter();
   const { id } = router.query;
   if(!id || typeof id === "object") return
-  const data = useGetProductByIdQuery({ variables: id })
-  if (data.isSuccess) {
+  const data = useGetProductByIdQuery({ variables: Number.parseInt(id) })
+  const {mutateAsync: postCartMutate} = usePostCartMutation()
+  const accessToken = getToken()?.access
+  if(!accessToken) return
+  const userData = useGetUserMeQuery({variables: {
+    accessToken
+  }})
+  if (data.isSuccess && userData.isSuccess) {
     const productData = data.data
-    return (
-      <>
-        <Head>
-          {/* ex) Your App Name | Page Name */}
-          <title>인코스런 커머스트랙 | 상품 상세보기</title>
-        </Head>
-        <MainLayout content={<ProductsDetailByIdPage productData={productData} />} />
-      </>
-    );
+    const user_id = userData.data.id
+      return (
+        <>
+          <Head>
+            {/* ex) Your App Name | Page Name */}
+            <title>인코스런 커머스트랙 | 상품 상세보기</title>
+          </Head>
+          <MainLayout content={<ProductsDetailByIdPage user_id={user_id} product_data={productData} />} />
+        </>
+      );
   }
 
   return (
