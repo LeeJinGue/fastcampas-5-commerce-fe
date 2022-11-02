f780f07f26064b74d5978948ac06697fd2b04be2
LeeJinGue <hippo9851@gmail.com>
2022-11-02 20:57:52 +0900

    fix: 개발/배포인지에 따라 redirect url 다르게 수정

--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,13 +1,7 @@
 import { CONFIG } from "@config";
-
+const isDev = CONFIG.ENV === 'development';
+const BASE = isDev ? `http://localhost:3000` : `https://fastcampus-5-commerce-fe.vercel.app`
 export const SOCIAL = {
   KAKAO_CLIENT_ID: CONFIG.AUTH_KAKAO_CLIENT_KEY,
-  KAKAO_CLIENT_ID_ME: CONFIG.AUTH_KAKAO_MYKEY,
-  NAVER_CLIENT_ID: '',
-  FACEBOOK_CLIENT_ID: '',
-  GOOGLE_CLIENT_ID: '',
-  APPLE_CLIENT_ID: '',
-  REDIRECT_URL_ME: 'http://127.0.0.1:3000/social_login/callback',
-  REDIRECT_URL: `http://localhost:3000/social_login/callback`,
-  
+  REDIRECT_URL: `${BASE}/social_login/callback`,
 };
