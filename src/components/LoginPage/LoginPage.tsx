ecdc9872480be0ed0560a51e96ff5df57979955f
LeeJinGue <hippo9851@gmail.com>
2022-11-02 20:56:35 +0900

    chore: LoginPage에서 필요없는 상수 삭제

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -4,16 +4,13 @@ import LogoWhiteIcon from '@components/common/New/@Icons/LogoWhite';
 import { CONFIG } from '@config';
 import { SOCIAL } from '@constants/social';
 import KakaoButton, { SocialType } from '@components/common/KakaoButton';
-
-interface LoginPageProps extends ChakraProps {}
-const REDIRECT_URL="http://localhost:3000/social_login/callback"
-const SOCIAL_REDIRECT_URL = `${CONFIG.DOMAIN}`;
-
 const SOCIAL_DATA: { social: SocialType; link: string } = 
   {
     social: 'kakao',
     link: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${SOCIAL.KAKAO_CLIENT_ID}&redirect_uri=${SOCIAL.REDIRECT_URL}&state=kakao`,
   }
+
+interface LoginPageProps extends ChakraProps {}
 
 const LoginPage = ({ ...basisProps }: LoginPageProps) => {
   return (
