9593f61c82077c1022fb43c1f0f6a9a715fbecea
LeeJinGue <hippo9851@gmail.com>
2022-11-14 18:06:45 +0900

    feat: Header의 Logo 수정
    - 로고 클릭 시 홈으로 이동하도록 추가
    - 헤더 맨 위에 고정되도록 수정
    - Logo Component Flex -> IconButton으로 변경
    - 위의 변경에 따라 필수 prop 추가

--- src/components/common/New/LogoComponent.tsx
+++ src/components/common/New/LogoComponent.tsx
@@ -1,4 +1,6 @@
-import { Flex, FlexProps } from '@chakra-ui/react'
+import { Flex, FlexProps, IconButton, IconButtonProps } from '@chakra-ui/react'
+import { ROUTES } from '@constants/routes'
+import { useRouter } from 'next/router'
 import React from 'react'
 import ExitIcon from '../@Icons/System/Exit'
 import LogoPrimaryIcon from './@Icons/LogoPrimary'
@@ -7,15 +9,24 @@ import LogoWhiteIcon from './@Icons/LogoWhite'
 const COMPONENT_WIDTH = "215px"
 const COMPONENT_HEIGHT = "80px"
 type LogoAlignType = "start" | "center"
-interface LogoComponentProps extends FlexProps{
-  align: LogoAlignType,
+interface LogoComponentProps extends IconButtonProps{
+  align: LogoAlignType
+
 }
 const LogoComponent = ({ align, ...props}:LogoComponentProps) => {
+  const route = useRouter()
+  const handleLogoClick = () => {
+    if(route.asPath === ROUTES.HOME) route.replace({pathname: ROUTES.HOME})
+    else route.push({pathname: ROUTES.HOME})
+  }
   return (
-    <Flex flexDir="column" w={COMPONENT_WIDTH} h={COMPONENT_HEIGHT} justifyContent="center"
-    alignItems={align} {...props}>
-      <LogoPrimaryIcon />
-    </Flex>
+    <IconButton
+    icon={<LogoPrimaryIcon />}
+    onClick={handleLogoClick}
+    bgColor="transparent"
+    flexDir="column" w={COMPONENT_WIDTH} h={COMPONENT_HEIGHT} justifyContent="center"
+    alignItems={align} {...props}>      
+    </IconButton>
   )
 }
 export default LogoComponent;
