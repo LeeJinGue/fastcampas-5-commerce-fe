d955c8bf5bac4791e3dcfdcdf491dc2886883ba7
LeeJinGue <hippo9851@gmail.com>
2023-01-26 22:42:19 +0900

    fix: 장바구니 Drawer에 장바구니 담기 완료 팝업 이벤트 추가
    - 팝업창에 필요한 장바구니 이동, 쇼핑 계속하기 이벤트 추가

--- src/components/ProductsDetailByIdPage/_fragments/DrawerBuy.tsx
+++ src/components/ProductsDetailByIdPage/_fragments/DrawerBuy.tsx
@@ -14,6 +14,7 @@ import {
   Flex,
   Input,
   Modal,
+  useDisclosure,
 } from '@chakra-ui/react';
 import PrimaryButton from '@components/common/New/PrimaryButton';
 import { useRouter } from 'next/router';
@@ -22,6 +23,8 @@ import CommerceCount from '@components/common/Card/CommerceCount';
 import { usePostCartItemMutation } from '@apis/cart/CartApi.mutation';
 import { useDispatch } from 'react-redux';
 import { addOrderItemType, orderItemSliceActions, orderItemType, setTotalType } from '@features/orderItem/orderItemSlice';
+import Popup from '@components/common/New/Popup';
+import { cart_popup_string } from '@constants/string';
 // 필요한 데이터: 상품, 유저, 장바구니
 interface DrawerBuyProductData {
   id: number;
@@ -33,23 +36,21 @@ interface DrawerBuyProps extends DrawerProps {
   productData: DrawerBuyProductData,
   cart_id: number,
 }
-
+const {bodyText, okText, cancelText} = cart_popup_string
 function DrawerBuy(props: Omit<DrawerBuyProps, 'children'>) {
+  const {onClose: drawerClose} = props
   const dispatch =  useDispatch()
   const {productData, cart_id:cartId} = props
   const {name, price, id:productId} = productData
   const route = useRouter()
   const {mutateAsync: cartItemMutate} = usePostCartItemMutation()
   const [count, setCount] = React.useState(1)
-
+  const { isOpen:isPopupOpen, onClose:popupClose, onOpen:popupOpen} = useDisclosure()
   const handleCartOnclick = () => {
     // 장바구니 버튼 onClick 이벤트 함수
     cartItemMutate({productId,cartId, count,}).then(res => {      
       console.log("# cartItemMutate success:",res)
-      route.push({
-        pathname:ROUTES.CART, 
-        query: {productId, count}
-      })
+      popupOpen()
     }).catch(error => {
       console.log("# cartItemMutate error:",error)
     })
@@ -69,7 +70,17 @@ function DrawerBuy(props: Omit<DrawerBuyProps, 'children'>) {
     dispatch(orderItemSliceActions.setTotal({totalCost:buyNowTotalCost, totalDeliveryCost:buyNowTotalDeliveryCost}))
     route.push({pathname:ROUTES.PAYMENT.MAIN})
   }
+  const handleContinueShopping = () => {
+    // 쇼핑 계속하기 onClick 이벤트 함수
+    popupClose()
+    drawerClose()
+  }
+  const moveToCartPage = () => {
+    // 장바구니 이동 onClick 이벤트 함수
+    route.push({pathname:ROUTES.CART})
+  }
   return (
+    <>
     <Drawer placement="bottom" {...props}>
       <DrawerOverlay  />
       <DrawerContent position="relative" w="375px" borderRadius="10px 10px 0 0">
@@ -90,6 +101,12 @@ function DrawerBuy(props: Omit<DrawerBuyProps, 'children'>) {
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
+    <Popup isOpen={isPopupOpen} onClose={popupClose} 
+    okMsg={okText} okOnclick={handleContinueShopping} 
+    bodyMsg={bodyText} 
+    cancelMsg={cancelText} cancelOnclick={moveToCartPage} 
+    children={''} />
+    </>
   );
 }
 
