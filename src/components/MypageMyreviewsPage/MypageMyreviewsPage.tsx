dc41a787f7f17e3822c9327edea255c4a9e2f0e0
LeeJinGue <hippo9851@gmail.com>
2023-02-01 18:27:13 +0900

    fix: 내 리뷰 목록 Pagination 에러 수정
    - 목록의 마지막 페이지 계산 로직 수정
    - 전체 결과를  slice해서 현재 페이지의 리뷰 목록을 사용합니다.

--- src/components/MypageMyreviewsPage/MypageMyreviewsPage.tsx
+++ src/components/MypageMyreviewsPage/MypageMyreviewsPage.tsx
@@ -29,17 +29,17 @@ function MypageMyreviewsViewPage({userData, ...basisProps }: MypageMyreviewsView
   if(isError) return <Text>리뷰 가져오기 에러</Text>
   if(!reviewData) return <Text>리뷰 데이터가 없습니다.</Text>
   const {results:reviewList, count:reviewCount} = reviewData
-  const lastPage = reviewList.length/5
-  const startPage = (page-1)*5
-  const endPage = page*5
-  const nowPageReviewList = reviewList.slice(startPage,endPage)
+  const lastPage = Math.floor((reviewCount / 4)) + 1
+  const startReviewIndex = (page-1) * 4
+  const endReviewIndex = (page * 4) - 1
+  const nowPageReviewList = reviewList.slice(startReviewIndex,endReviewIndex)
   return (
     <Flex pt={LAYOUT.HEADER.HEIGHT} pb="80px" flexDir="column" bgColor="white" w="375px"{...basisProps}>
       <Text px="16px" mt="50px" textStyle="titleLarge" textColor="black">내 상품 리뷰</Text>
       <Text px="16px" mt="80px" textStyle="title" textColor="black">{"총 "}<Text as="span" textColor="primary.500">{reviewCount}</Text>건</Text>
       <Flex flexDir="column" mt="30px">
-        {nowPageReviewList && nowPageReviewList.map((value) => {
-          return <Review iscomment={false} reviewData={value}/>
+        {nowPageReviewList && nowPageReviewList.map((reviewData) => {
+          return <Review key={reviewData.id} iscomment={false} reviewData={reviewData}/>
         })}
       </Flex>
       <Pagination page={page} setPage={setPage} mt="50px" lastPage={lastPage} />
