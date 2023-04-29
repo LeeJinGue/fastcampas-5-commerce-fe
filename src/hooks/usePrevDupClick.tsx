fb91ffe8b43fc624fb7e2524fc69bc7c17ebf417
LeeJinGue <hippo9851@gmail.com>
2023-04-29 20:22:15 +0900

    feat: onSubmit 중복 제출 방지를 위한 커스텀 훅 추가
    - usePrevDupSubmit 훅 추가
    - setTimeout 오버로딩 때문에 state 타입 NodeJS.Timeout -> number로 수정

--- src/hooks/usePrevDupClick.tsx
+++ src/hooks/usePrevDupClick.tsx
@@ -3,6 +3,11 @@ export type usePrevDupClickProp = {
   callBack: () => void,
   time?: number,
 }
+export type usePrevDupSubmitProp = {
+  callBack:React.FormEventHandler<HTMLDivElement> | undefined
+  time?: number,
+}
+
 
 export const usePrevDupClick = ({callBack, time = 300}:usePrevDupClickProp) => {
   const [id, setId] = React.useState<NodeJS.Timeout>()
@@ -13,4 +18,16 @@ export const usePrevDupClick = ({callBack, time = 300}:usePrevDupClickProp) => {
     setId(setTimeout(callBack, time))
   }
   return {oneOnclick: onClick}
+}
+export const usePrevDupSubmit = ({callBack, time = 300}:usePrevDupSubmitProp) => {
+  const [id, setId] = React.useState<number>()
+  const onSubmit = () => {
+    if(id){
+      clearTimeout(id)      
+    }
+    if(callBack){
+      setId(setTimeout(callBack, time))
+    }
+  }
+  return {oneOnSubmit: onSubmit}
 }
